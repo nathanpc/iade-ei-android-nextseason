c5896afddbccc6c72fa5003531021267fdd1b50c
Nathan Campos <nathan@innoveworkshop.com>
2024-09-25 10:47:28 +0100

    Initial commit

---
+++ app/src/main/java/pt/iade/ei/nextseason/ui/components/RatedContentListItem.kt
@@ -0,0 +1,14 @@
+package pt.iade.ei.nextseason.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RatedContentListItem() {
+    Text("Hello World!")
+}
+
+@Composable
+fun RatedContentListItemPreview() {
+    RatedContentListItem()
+}
